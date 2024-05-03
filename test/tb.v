d24de2c263619504089989add3f8ef4139d7dc7d
navya00609 <navyaaa81@gmail.com>
2024-05-03 16:41:06 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tkm_ic user_project (
+  tt_um_n user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
